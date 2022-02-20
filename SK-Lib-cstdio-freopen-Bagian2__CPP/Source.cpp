71ae2746b3e27b8744a2122bebdad2860b12829a
Rizky Khapidsyah <rizkykhapidsyah@gmail.com>
2022-02-20 07:03:12 +0700

    Add project files.

---
+++ SK-Lib-cstdio-freopen-Bagian2__CPP/Source.cpp
@@ -0,0 +1,21 @@
+#pragma warning(disable:4996)
+
+#include <stdio.h>
+#include <conio.h>
+
+/*
+	Source by GeeksForGeeks
+	Modified For Learn by RK
+	I.D.E : VS2019
+*/
+
+int main() {
+#ifndef ONLINE_JUDGE
+
+	freopen("input.txt", "r", stdin);
+	freopen("output.txt", "w", stdout);
+
+#endif
+	_getch();
+	return 0;
+}
